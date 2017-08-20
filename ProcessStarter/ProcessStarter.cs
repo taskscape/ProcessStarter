220c67c11a56f3799ab19ef375f148a289d2ca02
Maciej Zagozda <maciej.zagozda@gmail.com>
2017-08-20 17:39:15 +0200

    Improvements catering for the LoadUserProfile

--- ProcessStarter/ProcessStarter.cs
+++ ProcessStarter/ProcessStarter.cs
@@ -29,14 +29,25 @@ namespace ProcessStarter
         public bool Initialise()
         {
 
-            string command = ConfigHelper.CommandName;
-
             IntPtr userToken = GetUserToken(UserName, Environment.MachineName, Password);
             if (userToken == IntPtr.Zero) return false;
 
-            Process process = CreateProcessAsUser(userToken, null, command);
+            IntPtr userRegToken = LoadUserProfile(userToken, UserName);
+            if (userRegToken == IntPtr.Zero) return false;
+
+            if (Win32.CreateEnvironmentBlock(out IntPtr envBlock, userToken, 0) == 0) return false;
+
+            Win32.SecurityAttributes sa = new Win32.SecurityAttributes();
+            sa.nLength = (UInt32) Marshal.SizeOf(sa);
+
+            Win32.Startupinfo si = new Win32.Startupinfo();
+            si.cb = (UInt32) Marshal.SizeOf(si);
+
+            Process process = CreateProcessAsUser(userToken, null, ConfigHelper.CommandName, sa, sa, false, 0, IntPtr.Zero, null, si);
             if (process == null) return false;
 
+            if (Win32.UnloadUserProfile(userToken, userRegToken) == 0) return false;
+            if (Win32.CloseHandle(envBlock) == 0) return false;
             if (!ReleaseToken(userToken)) return false;
 
             process.WaitForExit();
@@ -44,11 +55,35 @@ namespace ProcessStarter
             return true;
         }
 
+        /// <exception cref="Exception">NetUserGetInfo failed.</exception>
+        private static Win32.UserInfo GetUserInfo(string username)
+        {
+            IntPtr bufptr = IntPtr.Zero;
+            uint code = Win32.NetUserGetInfo(Environment.MachineName, username, 4, out bufptr);
+            if (code != 0)
+            {
+                Program.Log.Error($"NetUserGetInfo failed with error code: {code}. See: NET_API_STATUS");
+                throw new Exception($"NetUserGetInfo failed with error code: {code}. See: NET_API_STATUS");
+            }
+
+            try
+            {
+                Win32.UserInfo userInfo = (Win32.UserInfo) Marshal.PtrToStructure(bufptr, typeof(Win32.UserInfo));
+                return userInfo;
+            }
+            catch (Exception ex)
+            {
+                Program.Log.Error($"GetUserInfo: Could not convert data to UserInfo. Exception: {ex.Message}");
+                throw;
+            }
+        }
+
         private static IntPtr GetUserToken(string username, string domain, string password)
         {
             IntPtr token = IntPtr.Zero;
 
-            if (Win32.LogonUser(username, domain, password, (UInt32)Win32.Logon32Type.Interactive, (UInt32)Win32.Logon32Provider.Default, out token) == 0)
+            if (Win32.LogonUser(username, domain, password, (UInt32) Win32.Logon32Type.Interactive,
+                    (UInt32) Win32.Logon32Provider.Default, out token) == 0)
             {
                 Program.Log.Error($"LogonUser failed with error code: {Marshal.GetLastWin32Error()}");
                 return IntPtr.Zero;
@@ -67,26 +102,54 @@ namespace ProcessStarter
             return true;
         }
 
+        private static IntPtr LoadUserProfile(IntPtr token, string username)
+        {
+            Win32.UserInfo uInfo;
+            try
+            {
+                uInfo = GetUserInfo(username);
+            }
+            catch (Exception ex)
+            {
+                Program.Log.Error(
+                    $"LoadUserProfile: Could not get user info for user {username}. Exception: {ex.Message}");
+                return IntPtr.Zero;
+            }
+
+            Win32.ProfileInfo pInfo = new Win32.ProfileInfo();
+            pInfo.dwSize = (UInt32) Marshal.SizeOf(pInfo);
+            pInfo.lpUserName = uInfo.usri4_name;
+            pInfo.lpProfilePath = uInfo.usri4_profile;
+
+            if (Win32.LoadUserProfile(token, ref pInfo) == 0)
+            {
+                Program.Log.Error($"LoadUserProfile failed with error code: {Marshal.GetLastWin32Error()}");
+                return IntPtr.Zero;
+            }
+
+            return pInfo.hProfile;
+        }
+
         private static Process CreateProcessAsUser(IntPtr token, string appName, string appArgs)
         {
             Win32.SecurityAttributes sa = new Win32.SecurityAttributes();
-            sa.nLength = (UInt32)Marshal.SizeOf(sa);
+            sa.nLength = (UInt32) Marshal.SizeOf(sa);
 
             Win32.Startupinfo si = new Win32.Startupinfo();
-            si.cb = (UInt32)Marshal.SizeOf(si);
+            si.cb = (UInt32) Marshal.SizeOf(si);
 
             return CreateProcessAsUser(token, appName, appArgs, sa, sa, false, 0, IntPtr.Zero, null, si);
         }
 
         private static Process CreateProcessAsUser(IntPtr token, string appName, string appArgs,
-                                                   Win32.SecurityAttributes processAttributes, Win32.SecurityAttributes threadAttributes,
-                                                   bool inheritHandles, UInt32 creationFlags, IntPtr environment, string currentDir,
-                                                   Win32.Startupinfo startupInfo)
+            Win32.SecurityAttributes processAttributes, Win32.SecurityAttributes threadAttributes,
+            bool inheritHandles, UInt32 creationFlags, IntPtr environment, string currentDir,
+            Win32.Startupinfo startupInfo)
         {
             Win32.ProcessInformation pi = new Win32.ProcessInformation();
-            
+
             if (Win32.CreateProcessAsUser(token, appName, appArgs, ref processAttributes, ref threadAttributes,
-               inheritHandles ? 1 : 0, creationFlags, environment, currentDir, ref startupInfo, out pi) == 0)
+                    inheritHandles ? 1 : 0, creationFlags, environment, currentDir, ref startupInfo, out pi) == 0)
             {
                 Program.Log.Error($"CreateProcessAsUser failed with error code: {Marshal.GetLastWin32Error()}");
                 return null;
@@ -94,9 +157,9 @@ namespace ProcessStarter
 
             try
             {
-                return Process.GetProcessById((Int32)pi.dwProcessId);
+                return Process.GetProcessById((Int32) pi.dwProcessId);
             }
-            catch(ArgumentException ex)
+            catch (ArgumentException ex)
             {
                 Program.Log.Error($"Process with id {pi.dwProcessId} does not exist. {ex.Message}");
                 return null;
--- ProcessStarter/Win32.cs
+++ ProcessStarter/Win32.cs
@@ -11,11 +11,11 @@ namespace ProcessStarter
         public struct Startupinfo
         {
             public UInt32 cb;
-            [MarshalAs(UnmanagedType.LPStr)]
+            [MarshalAs(UnmanagedType.LPTStr)]
             public String lpReserved;
-            [MarshalAs(UnmanagedType.LPStr)]
+            [MarshalAs(UnmanagedType.LPTStr)]
             public String lpDesktop;
-            [MarshalAs(UnmanagedType.LPStr)]
+            [MarshalAs(UnmanagedType.LPTStr)]
             public String lpTitle;
             public UInt32 dwX;
             public UInt32 dwY;
@@ -50,19 +50,71 @@ namespace ProcessStarter
             public Int32 bInheritHandle;
         }
 
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ProfileInfo
+        {
+            public UInt32 dwSize;
+            public UInt32 dwFlags;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpUserName;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpProfilePath;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpDefaultPath;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpServerName;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpPolicyPath;
+            public IntPtr hProfile;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct UserInfo
+        {
+            public string usri4_name;
+            public string usri4_password;
+            public UInt32 usri4_password_age;
+            public UInt32 usri4_priv;
+            public string usri4_home_dir;
+            public string usri4_comment;
+            public UInt32 usri4_flags;
+            public string usri4_script_path;
+            public UInt32 usri4_auth_flags;
+            public string usri4_full_name;
+            public string usri4_usr_comment;
+            public string usri4_parms;
+            public string usri4_workstations;
+            public UInt32 usri4_last_logon;
+            public UInt32 usri4_last_logoff;
+            public UInt32 usri4_acct_expires;
+            public UInt32 usri4_max_storage;
+            public UInt32 usri4_units_per_week;
+            public IntPtr usri4_logon_hours;
+            public UInt32 usri4_bad_pw_count;
+            public UInt32 usri4_num_logons;
+            public string usri4_logon_server;
+            public UInt32 usri4_country_code;
+            public UInt32 usri4_code_page;
+            public IntPtr usri4_user_sid;
+            public UInt32 usri4_primary_group_id;
+            public string usri4_profile;
+            public string usri4_home_dir_drive;
+            public UInt32 usri4_password_expired;
+        }
+
         #endregion
 
         #region "ENUMS"
 
         public enum Logon32Type
         {
-            Interactive         = 2,
-            Network             = 3,
-            Batch               = 4,
-            Service             = 5,
-            Unlock              = 7,
-            NetworkClearText    = 8,
-            NewCredentials      = 9
+            Interactive = 2,
+            Network = 3,
+            Batch = 4,
+            Service = 5,
+            Unlock = 7,
+            NetworkClearText = 8,
+            NewCredentials = 9
         }
 
         public enum Logon32Provider
@@ -76,26 +128,38 @@ namespace ProcessStarter
 
         #region "FUNCTIONS (P/INVOKE)"
 
-        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Ansi, EntryPoint = "LogonUserA")]
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         public extern static int LogonUser(string lpszUsername,
-                                             string lpszDomain,
-                                             string lpszPassword,
-                                             UInt32 dwLogonType,
-                                             UInt32 dwLogonProvider,
-                                             out IntPtr phToken);
+            string lpszDomain,
+            string lpszPassword,
+            UInt32 dwLogonType,
+            UInt32 dwLogonProvider,
+            out IntPtr phToken);
 
-        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Ansi, EntryPoint = "CreateProcessAsUserA")]
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         public extern static int CreateProcessAsUser(IntPtr hToken,
-                                                       string lpApplicationName,
-                                                       string lpCommandLine,
-                                                       ref SecurityAttributes lpProcessAttributes,
-                                                       ref SecurityAttributes lpThreadAttributes,
-                                                       Int32 bInheritHandles,
-                                                       UInt32 dwCreationFlags,
-                                                       IntPtr lpEnvironment,
-                                                       string lpCurrentDirectory,
-                                                       ref Startupinfo lpStartupInfo,
-                                                       out ProcessInformation lpProcessInformation);
+            string lpApplicationName,
+            string lpCommandLine,
+            ref SecurityAttributes lpProcessAttributes,
+            ref SecurityAttributes lpThreadAttributes,
+            Int32 bInheritHandles,
+            UInt32 dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            ref Startupinfo lpStartupInfo,
+            out ProcessInformation lpProcessInformation);
+
+        [DllImport("Netapi32.dll", CharSet = CharSet.Unicode)]
+        public extern static uint NetUserGetInfo(string servername, string username, UInt32 level, out IntPtr bufptr);
+
+        [DllImport("Userenv.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public extern static int LoadUserProfile(IntPtr hToken, ref ProfileInfo lpProfileInfo);
+
+        [DllImport("Userenv.dll", SetLastError = true)]
+        public extern static int UnloadUserProfile(IntPtr hToken, IntPtr hProfile);
+
+        [DllImport("Userenv.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public extern static int CreateEnvironmentBlock(out IntPtr lpEnvironment, IntPtr hToken, int bInherit);
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public extern static int CloseHandle(IntPtr hObject);
